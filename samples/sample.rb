4ca384824b401e7044efa5cc75aa2653f6c4d7f8
Dave Meppelink <davemepp@gmail.com>
2012-02-12 12:07:11 -0500

    For https://github.com/nuodb/nuodb-drivers/issues/3 Rename "SampleUsers" to "Users". Rename "SampleAddrs" to "Addrs". Use Type.table_name rather than multiple constants. Simply drop statements.

--- samples/sample.rb
+++ samples/sample.rb
@@ -12,51 +12,43 @@ ActiveRecord::Base.establish_connection(
 
 puts "Create tables..."
 
-def drop(name)
-  begin
-    ActiveRecord::Schema.drop_table(name)
-  rescue
-    nil
+class User < ActiveRecord::Base
+  has_one :addr, :class_name => 'Addr'
+  def to_s
+    return "User(#{id}), Username: #{user_name}, Name: #{first_name} #{last_name}, #{admin ? "admin" : "member"}\n" +
+    "  Address: #{addr}\n"
   end
 end
 
-drop(:sample_users)
-drop(:sample_addrs)
+class Addr < ActiveRecord::Base
+  belongs_to :User
+  def to_s
+    return "Addr(#{id}:#{user_id}) Street: #{street} City: #{city} Zip: #{zip}"
+  end
+end
+
+ActiveRecord::Schema.drop_table(User.table_name) rescue nil
+ActiveRecord::Schema.drop_table(Addr.table_name) rescue nil
 
 ActiveRecord::Schema.define do
-  create_table :sample_users do |t|
+  create_table User.table_name do |t|
     t.string :first_name, :limit => 20
     t.string :last_name, :limit => 20
     t.string :email, :limit => 20
     t.string :user_name, :limit => 20
     t.boolean :admin
   end
-  create_table :sample_addrs do |t|
-    t.integer :sample_user_id
+  create_table Addr.table_name do |t|
+    t.integer :user_id
     t.string :street, :limit => 20
     t.string :city, :limit => 20
     t.string :zip, :limit => 6
   end
 end
 
-class SampleUser < ActiveRecord::Base
-  has_one :addr, :class_name => 'SampleAddr'
-  def to_s
-    return "SampleUser(#{id}), Username: #{user_name}, Name: #{first_name} #{last_name}, #{admin ? "admin" : "member"}\n" +
-    "  Address: #{addr}\n"
-  end
-end
-
-class SampleAddr < ActiveRecord::Base
-  belongs_to :SampleUser
-  def to_s
-    return "SampleAddr(#{id}:#{sample_user_id}) Street: #{street} City: #{city} Zip: #{zip}"
-  end
-end
-
 puts "Create user records..."
 
-@u = SampleUser.create do |u|
+@u = User.create do |u|
   u.first_name = "Fred"
   u.last_name = "Flintstone"
   u.email = "fredf@example.com"
@@ -72,7 +64,7 @@ end
 
 puts "Created #{@u}"
 
-@u = SampleUser.create do |u|
+@u = User.create do |u|
   u.first_name = "Barney"
   u.last_name = "Rubble"
   u.email = "barney@example.com"
@@ -90,14 +82,14 @@ puts "Created #{@u}"
 
 puts "Print user records..."
 
-puts "Found #{SampleUser.count} records:"
-SampleUser.find do |entry|
+puts "Found #{User.count} records:"
+User.find do |entry|
   puts entry
 end
 
 puts "Modify user records..."
 
-SampleUser.all.each do |entry|
+User.all.each do |entry|
   entry.first_name = entry.first_name.upcase
   entry.last_name = entry.last_name.upcase
   entry.admin = !entry.admin
@@ -108,8 +100,8 @@ end
 
 puts "Print user records..."
 
-puts "Found #{SampleUser.count} records:"
-SampleUser.find_each do |entry|
+puts "Found #{User.count} records:"
+User.find_each do |entry|
   puts entry
 end
 
