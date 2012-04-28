43090ccd2d1f1100c4d35fa902e21627db072109
Dave Meppelink <davemepp@gmail.com>
2012-04-28 12:55:33 -0400

    First round of build cleanup for dbd-nuodb.

---
+++ dbd-nuodb/README.md
@@ -0,0 +1,6 @@
+= NuoDB Database Driver
+
+== DESCRIPTION
+
+This is the NuoDB DBD gem for Ruby. It integrates the nuodb gem into dbi.
+
--- dbd-nuodb/ext/nuodb/nuodb.cpp
+++
@@ -1,9 +0,0 @@
-#include <ruby.h>
-
-extern "C"
-VALUE Init_nuodb(void)
-{
-    rb_require("dbi");
-    
-    VALUE nuoModule = rb_define_module("NuoDB");
-}
