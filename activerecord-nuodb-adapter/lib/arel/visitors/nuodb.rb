f920a7ce03a502dfe366ed547eaf55a197dabd01
Robert Buck <rbuck@nuodb.com>
2012-09-17 10:45:58 -0400

    Fix AREL syntax.

--- activerecord-nuodb-adapter/lib/arel/visitors/nuodb.rb
+++ activerecord-nuodb-adapter/lib/arel/visitors/nuodb.rb
@@ -26,42 +26,30 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-require 'arel'
-
 module Arel
 
   module Visitors
+
     class NuoDB < Arel::Visitors::ToSql
 
       private
 
-      def visit_Arel_Nodes_Offset(o)
-        "WHERE [__rnt].[__rn] > (#{visit o.expr})"
+      def visit_Arel_Nodes_SelectStatement o
+        [
+            (visit(o.with) if o.with),
+            o.cores.map { |x| visit_Arel_Nodes_SelectCore x }.join,
+            ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
+            (visit(o.offset) if o.offset),
+            (visit(o.limit) if o.limit),
+            (visit(o.lock) if o.lock),
+        ].compact.join ' '
       end
 
       def visit_Arel_Nodes_Limit(o)
         "FETCH FIRST #{visit o.expr} ROWS ONLY"
       end
-
-      def visit_Arel_Nodes_Lock(o)
-        visit o.expr
-      end
-      
-      def visit_Arel_Nodes_Ordering(o)
-        if o.respond_to?(:direction)
-          "#{visit o.expr} #{o.ascending? ? 'ASC' : 'DESC'}"
-        else
-          visit o.expr
-        end
-      end
-      
-      def visit_Arel_Nodes_Bin(o)
-        "#{visit o.expr} #{@connection.cs_equality_operator}"
-      end
-
     end
+
   end
 
 end
-
-Arel::Visitors::VISITORS['nuodb'] = Arel::Visitors::NuoDB
